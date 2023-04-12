a5af09f6f8f5edb85c5fb7fef118d5473ea710b1
Caio França <caiobdafranca@gmail.com>
2023-04-11 23:24:22 -0300

    🐞fix: Corrigir build

--- src/components/Dashboard/PredictionCondition/index.tsx
+++ src/components/Dashboard/PredictionCondition/index.tsx
@@ -5,27 +5,6 @@ import styled from "styled-components";
 import { IConfig } from "../../../types/Gauge";
 import { colors } from "../../../theme/colors";
 
-const gaugeConfig: IConfig = {
-  height: 40,
-  width: 150,
-  center: ["50%", "85%"],
-  size: "245%",
-  indicators: {
-    good: {
-      min: 0.9,
-      max: 0.98,
-    },
-    bad: {
-      min: 0.98,
-      max: 1,
-    },
-  },
-  yAxisConfig: {
-    min: 0.9,
-    max: 1,
-  },
-};
-
 export default function PredictionCondition() {
   const [compressorData, setCompressorData] = useState<number[]>([
     0.952, 0.951, 0.964, 0.954, 0.961, 0.971, 0.988, 0.981, 0.973, 0.952, 0.951,
